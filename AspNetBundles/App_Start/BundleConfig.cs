862f9ab191488eab3f3f755e3e8f68ecfa935b25
devwz <wellingtonantonio_zaneze@live.com>
2018-04-30 17:17:02 -0300

    Add BundleConfig and references.

---
+++ AspNetBundles/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace AspNetBundles
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/Script.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/Style.css"));
+        }
+    }
+}
